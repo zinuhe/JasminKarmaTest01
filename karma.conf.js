7f5d7ddfc4b10004337d7af5b0a32ba12b0422dc
Jimmy <zinuhe@outlook.com>
2019-08-30 17:17:43 -0400

    Comment on karma config for FF

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      // require('karma-firefox-launcher'), // To add Firefox
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,7 +26,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    // browsers: ['Chrome'],
+    // browsers: ['Chrome', 'Firefox'], // To add Firefox
     singleRun: false,
     restartOnFileChange: true
   });
